473443844f3e7b98c1a25f6010d6d7e4292a14d9
slowik <aleksandra.slowik88@gmail.com>
2020-09-09 15:37:03 +0200

    server.php table name correction

--- login must be split/server.php
+++ login must be split/server.php
@@ -46,7 +46,7 @@ if (isset($_POST['reg_user'])) {
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
-  	$query = "INSERT INTO register (username, email, password) 
+  	$query = "INSERT INTO registration (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
