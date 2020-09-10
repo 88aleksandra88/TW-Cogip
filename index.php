45085b32cc542c3c9462b3af8c92c8faefa5d2b5
Nicode.IO <nicolas.denoel@gmail.com>
2020-09-10 08:37:32 +0200

    Revert index.php : remove switch case

--- index.php
+++ index.php
@@ -5,65 +5,37 @@ error_reporting(E_ALL);
 
 require('./controller/controller.php');
 
-if(isset($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'listCompanies':
-            listCompanies();
-            break;
-    
-        case 'listContacts':
-            listContacts();
-            break;
-    
-        case 'listInvoices':
-            listInvoices();
-            break;
-    
-        case 'detailCompany':
-            detailCompany();
-            break;
-    
-        case 'detailContact':
-            detailContact();
-            break;
-    
-        case 'detailInvoice':
-            detailInvoice();
-            break;
-        
-        case 'newCompany':
-            newCompany();
-            break;
-    
-        case 'newContact':
-            newContact();
-            break;
-    
-        case 'newInvoice':
-            newInvoice();
-            break;
-        
-        default:
-            echo "ERROR: action undefined :( </br>
-                (Check that you didn't make a typo in the URL ;)";
-            break;
-    }
-    else if($_GET['action'] == "listContacts") {
+if(isset($_GET['action'])) 
+{
+    if($_GET['action'] == "listContacts") 
+    {
         listContacts();
     }
-    else if($_GET['action'] == "listInvoices") {
+    else if($_GET['action'] == "listInvoices") 
+    {
         listInvoices();
     }
-    else if($_GET['action'] == "adminPanel"){
+    else if($_GET['action'] == "adminPanel")
+    {
         adminPanel();
     }
-    else if($_GET['action'] == "detailCompany") {
+    else if($_GET['action'] == "detailCompany") 
+    {
         detailCompany();
     }
-    else if($_GET['action'] == "detailContact") {
+    else if($_GET['action'] == "detailContact") 
+    {
         detailContact();
     }
-    else if($_GET['action'] == "detailInvoice") {
-        detailInvoice();
+    else if($_GET['action'] == "newCompany") 
+    {
+        newContact();
+    }
+    else if($_GET['action'] == "newInvoice") 
+    {
+        newInvoice();
+    } else {
+        echo "ERROR: action undefined :( </br>
+                (Check that you didn't make a typo in the URL ;)";
     }
 }
